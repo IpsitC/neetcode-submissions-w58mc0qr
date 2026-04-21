f40d9b0448dd9975a8d29d4ec8412ad712729d1f
Ipsitneel Chaudhuri <142912204+IpsitC@users.noreply.github.com>
2026-04-21 11:03:18 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cpp
@@ -0,0 +1,16 @@
+//With map
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+    map<int, int> mpp;
+    for(int i = 0; i < nums.size(); i++){
+        mpp[nums[i]]++;
+    }
+    for(auto pair : mpp){
+        if(pair.second > 1){
+            return true;
+        }
+    }
+    return false;    
+    }
+};
