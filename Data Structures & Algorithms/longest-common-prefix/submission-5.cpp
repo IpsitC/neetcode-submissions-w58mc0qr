f42709974851f528f51224dc791ec748345a299d
Ipsitneel Chaudhuri <142912204+IpsitC@users.noreply.github.com>
2026-04-22 10:58:09 +0530

    Add: longest-common-prefix - submission-5

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-5.cpp
@@ -0,0 +1,20 @@
+//Sorted & comparing ith elements wise and return len
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+    if(strs.empty()) return "";
+    sort(strs.begin(), strs.end());
+    string first = strs[0];
+    string last = strs[strs.size() - 1];
+    int minLength = min(first.size(), last.size());
+    string ans = "";
+
+    for(int i = 0; i < minLength; i++){
+        if(first[i] != last[i]){
+            return ans;
+        }
+        ans += first[i];
+    }
+    return ans;    
+    }
+};
